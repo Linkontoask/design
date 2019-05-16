deede7f12db4d48daec114fc8a5a3417b02fa5f6
Linkontoask <18875142896@163.com>
2019-05-16 22:19:49 +0800

    add collection

---
+++ front/src/components/popup/collection/foodCollection.vue
@@ -0,0 +1,29 @@
+<template>
+	<transition name="">
+		<div class="food-collection"></div>
+	</transition>
+</template>
+
+<script>
+	export default {
+		name: 'houseCollection',
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		beforeMount() {
+
+		},
+		activated() {
+
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	
+</style>
---
+++ front/src/components/popup/collection/houseCollection.vue
@@ -0,0 +1,29 @@
+<template>
+	<transition name="">
+		<div class="house-collection"></div>
+	</transition>
+</template>
+
+<script>
+	export default {
+		name: 'houseCollection',
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		beforeMount() {
+
+		},
+		activated() {
+
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	
+</style>
